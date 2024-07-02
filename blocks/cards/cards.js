baff7ee353b3f8dfc5aeae8ebf7a0599f88cb17c
Danial Kafeel <danialkafeel@gmail.com>
2024-07-02 11:27:51 +0530

    Update cards.js

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,10 +1,14 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
-
 export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
+    // Create header if needed
+    const headerDiv = document.createElement('div');
+    headerDiv.className = 'cards-card-header';
+    headerDiv.innerText = 'Header Text'; // Replace with dynamic text if available
+    li.append(headerDiv);
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
